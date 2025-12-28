7ed6b09af31fd75a317b134fe22f06072617e893
djagtap100 <djagtap100@gmail.com>
2025-12-28 15:22:03 +0400

    assignanb

--- src/main/java/assignmentswapvariables.java
+++ src/main/java/assignmentswapvariables.java
@@ -1,28 +1,13 @@
 public class assignmentswapvariables {
     public static void main(String[] args) {
-        int i= 7;
-        int j= 10;
-//        int i= 7;
-//        int j= 10;
-//
-//        i=i^j;
-//        j=i^j;
-//        i=i^j;
-//
-//        System.out.println(i);
-//        System.out.println(j);
-//    }
         int a= 10;
         int b= 7;
 
-        i=i^j;
-        j=i^j;
-        i=i^j;
         a=a^b;
         b=a^b;
         a=a^b;
 
-        System.out.println(i);
-        System.out.println(j);
+        System.out.println("a ="+ a);
+        System.out.println("b ="+ b);
     }
 }
